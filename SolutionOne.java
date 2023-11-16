a8ee51516bece0eaa42a2e0f0c577841feae3b17
Bapboop <robertpopphan@gmail.com>
2023-11-16 12:42:57 -0800

    Add summarize portion

---
+++ SolutionOne.java
@@ -0,0 +1,20 @@
+//  SEAT METHOD:
+/* Summarize:
+*  We are given an array that has values. Every element will appear twice except one value.
+*  Requires O(n) runtime and O(1) space complexity.
+*/
+
+/* Examples
+* 
+*/
+
+/* Approach
+* 
+*/
+
+/* Test:
+ * 
+ */
+
+class SolutionOne {
+}
