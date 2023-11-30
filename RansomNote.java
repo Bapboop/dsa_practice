c18dda15e9a6974c558c2a0bb3d3ef264ef48ad9
Bapboop <robertpopphan@gmail.com>
2023-11-30 09:37:49 -0800

    Copy problem and summarize

---
+++ RansomNote.java
@@ -0,0 +1,24 @@
+/*
+ *  Given two strings ransomNote and magazine, return true if 
+ *  ransomNote can be constructed by using the letters from magazine
+ *  and false otherwise.
+ *  
+ * Each letter in magazine can only be used once in ransomNote
+ * 
+ * EX: ransomNote = "a", magazine = "b" => False
+ * ransomNote = "aa", magazine = "ab" => False
+ * ransomNote = "aa", magazine = "aab" => True
+ * 
+ * Constraints:
+ * 1 <= ransomNote.length, magazine.length <= 10^5
+ * ransomNote and magazine consists of lowercase English letters
+ */
+
+/* 
+ *  Summarize: Given two strings (ransomNote & magazine), ransomNote must be 
+ *  able to be constructed from the letters of magazine (Letters in magazine can only be used once.)
+ * 
+ */
+public class RansomNote {
+    
+}
